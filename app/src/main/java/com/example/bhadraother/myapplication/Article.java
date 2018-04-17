b2826b18b4f528239686a60637d52ddd339f336f
kvell36 <kvell36@gmail.com>
2018-04-16 23:16:43 -0400

    Created CardView

---
+++ app/src/main/java/com/example/bhadraother/myapplication/Article.java
@@ -0,0 +1,12 @@
+package com.example.bhadraother.myapplication;
+
+/**
+ * Created by kvell on 4/16/2018.
+ */
+
+public class Article {
+    String title="";
+    String author="";
+    String date="";
+    String url="";
+}
--- app/src/main/java/com/example/bhadraother/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bhadraother/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.bhadraother.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Article> articles;
+    CardView articleView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        articleView=(CardView)findViewById(R.id.ArticleView);
     }
 }
